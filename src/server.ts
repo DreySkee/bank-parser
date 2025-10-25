7ba35e5cd17ef300bc2a50b0b911cdaae5d727aa
Andrey Pokrovskiy <andreypkr@gmail.com>
2025-10-25 16:07:16 -0400

    add default get route

--- src/server.ts
+++ src/server.ts
@@ -11,5 +11,7 @@ app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 app.use("/api", uploadRoute);
 
+app.get("/", (_, res) => res.send("✅ Bank Parser backend is running!"));
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`✅ Server running on port ${port}`));
